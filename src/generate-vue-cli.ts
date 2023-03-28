b86d87e88d37c1b4e73c806721b3153bfffa5f62
Daniele Sabre <daniele.sabre@jakala.com>
2023-03-28 15:14:19 +0200

    fix: added recursive creation folders

--- src/generate-vue-cli.ts
+++ src/generate-vue-cli.ts
@@ -5,7 +5,7 @@ interface IArgs {
     dir: string;
     name: string;
     template?: string;
-};
+}
 
 const getArgs = () => {
     const args: string[] = process.argv.slice(2);
@@ -29,11 +29,11 @@ const args = getArgs();
 
 // check if dir and name are provided
 let hasErrors = false;
-if(!args.dir){
+if (!args.dir) {
     console.log(red(`Directory param not provided (pass it with --dir=[...]).\n`));
     hasErrors = true;
 }
-if(!args.name){
+if (!args.name) {
     console.log(red(`Component name param not provided (pass it with --name=[...]).\n`));
     hasErrors = true;
 }
@@ -42,14 +42,14 @@ if (hasErrors) {
 }
 
 // create directory if not exists
-if(!fs.existsSync(args.dir)){
-    fs.mkdirSync(args.dir);
+if (!fs.existsSync(args.dir)) {
+    fs.mkdirSync(args.dir, {recursive: true});
 }
 
 const path = `${args.dir}/${args.name}.vue`;
 const getTemplate = () => {
     // if template path is passed, than use it
-    if(args.template){
+    if (args.template) {
         return fs.readFileSync(args.template).toString().split('[[COMPONENT_NAME]]').join(args.name);
     }
 

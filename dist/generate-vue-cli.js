38b458441782a70fbdb9c6ef406006f3acc02e92
Daniele Sabre <daniele.sabre@gmail.com>
2023-09-08 12:33:00 +0200

    fix: added shebang note to executable

--- dist/generate-vue-cli.js
+++ dist/generate-vue-cli.js
@@ -1,9 +1,5 @@
-"use strict";
-var _a, _b;
-const fs = require('fs');
-const { red, green, bold, yellow } = require('chalk');
-if (process.argv.slice(2).indexOf('--help') > -1 || process.argv.slice(2).indexOf('-h') > -1) {
-    console.log(`Usage: npx @dsabre/generate-vue-cli [flags]
+#!/usr/bin/env node
+"use strict";var _a,_b;const fs=require("fs"),{red,green,bold,yellow}=require("chalk");(process.argv.slice(2).indexOf("--help")>-1||process.argv.slice(2).indexOf("-h")>-1)&&(console.log(`Usage: npx @dsabre/generate-vue-cli [flags]
 
 Generate a Vue component.
 
@@ -17,57 +13,15 @@ Flags:
     -h, --help   Display this help message and exit.
 
 Example:
-    npx @dsabre/generate-vue-cli --dir="src/components" --name="HelloWorld" --prefix="Prefix" --suffix="Suffix"`);
-    process.exit();
-}
-const getArgs = () => {
-    const args = process.argv.slice(2);
-    const ret = {
-        dir: '',
-        name: ''
-    };
-    args.forEach((arg) => {
-        const argInfo = arg.split('=');
-        const k = argInfo[0].split('--').join('').trim();
-        const v = argInfo[1].trim();
-        ret[k] = v;
-    });
-    return ret;
-};
-const args = getArgs();
-let hasErrors = false;
-if (!args.dir) {
-    console.log(red(`Directory param not provided (pass it with --dir=[...]).\n`));
-    hasErrors = true;
-}
-if (!args.name) {
-    console.log(red(`Component name param not provided (pass it with --name=[...]).\n`));
-    hasErrors = true;
-}
-if (hasErrors) {
-    process.exit(1);
-}
-if (!fs.existsSync(args.dir)) {
-    fs.mkdirSync(args.dir, { recursive: true });
-}
-args.name = `${(_a = args.prefix) !== null && _a !== void 0 ? _a : ''}${args.name}${(_b = args.suffix) !== null && _b !== void 0 ? _b : ''}`;
-const path = `${args.dir}/${args.name}.vue`;
-const getTemplate = () => {
-    if (args.template) {
-        return fs.readFileSync(args.template).toString().split('[[COMPONENT_NAME]]').join(args.name);
-    }
-    return `<script setup>
+    npx @dsabre/generate-vue-cli --dir="src/components" --name="HelloWorld" --prefix="Prefix" --suffix="Suffix"`),process.exit());const getArgs=()=>{const n=process.argv.slice(2),e={dir:"",name:""};return n.forEach(o=>{const t=o.split("="),r=t[0].split("--").join("").trim(),s=t[1].trim();e[r]=s}),e},args=getArgs();let hasErrors=!1;args.dir||(console.log(red(`Directory param not provided (pass it with --dir=[...]).
+`)),hasErrors=!0),args.name||(console.log(red(`Component name param not provided (pass it with --name=[...]).
+`)),hasErrors=!0),hasErrors&&process.exit(1),fs.existsSync(args.dir)||fs.mkdirSync(args.dir,{recursive:!0}),args.name=`${(_a=args.prefix)!==null&&_a!==void 0?_a:""}${args.name}${(_b=args.suffix)!==null&&_b!==void 0?_b:""}`;const path=`${args.dir}/${args.name}.vue`,getTemplate=()=>args.template?fs.readFileSync(args.template).toString().split("[[COMPONENT_NAME]]").join(args.name):`<script setup>
 const msg = '${args.name} component';
-</script>
+<\/script>
 
 <template>
     <div>{{ msg }}</div>
 </template>
-`;
-};
-if (fs.existsSync(path)) {
-    console.log(red(`Component ${bold(yellow(path))} already exists.\n`));
-    process.exit(1);
-}
-fs.writeFileSync(path, getTemplate(), { flag: 'wx' });
-console.log(green(`Component ${bold(yellow(path))} successfully created.\n`));
+`;fs.existsSync(path)&&(console.log(red(`Component ${bold(yellow(path))} already exists.
+`)),process.exit(1)),fs.writeFileSync(path,getTemplate(),{flag:"wx"}),console.log(green(`Component ${bold(yellow(path))} successfully created.
+`));
